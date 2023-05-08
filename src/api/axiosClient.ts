2bd6ae20d5af29a23c15f23ea8fab0fe8fcf017e
hsuifang <whalekiller03@gmail.com>
2023-05-09 00:48:32 +0800

    feat: Add 用戶基本資料查看及編輯

--- src/api/axiosClient.ts
+++ src/api/axiosClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosClient = axios.create({
-  baseURL: process?.env?.API_URL ? process.env.API_URL : '',
+  baseURL: process?.env?.NEXT_PUBLIC_API_URL ? process.env.NEXT_PUBLIC_API_URL : '',
   timeout: 5000,
   headers: {
     'Content-Type': 'application/json',
