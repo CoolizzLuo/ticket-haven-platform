8ada83e35b3f766183243497fbc7f9aeb8cd2556
Enzo <enzo.lo@cloudlytics.me>
2023-05-27 14:29:13 +0800

    feat: Api management

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -1,3 +1,20 @@
-const api = {};
+import { BaseResponse } from '@/lib/api/types/baseResponse';
+import { UserSinginReq, UserSinginRes } from '@/lib/api/types/userSignin';
+import { UserSingupReq } from '@/lib/api/types/userSignup';
+import httpClient from './httpClient';
+
+interface API<Req = void, Res = void> {
+  (req?: Req, options?: RequestInit): Promise<BaseResponse<Res>>;
+}
+
+interface APIEndpoints {
+  signin: API<UserSinginReq, UserSinginRes>;
+  signup: API<UserSingupReq>;
+}
+
+const api: APIEndpoints = {
+  signin: httpClient.post('/user/signin'),
+  signup: httpClient.post('/user/signup'),
+};
 
 export default api;
---
+++ src/lib/api/types/userSignin.ts
@@ -0,0 +1,8 @@
+export type UserSinginReq = {
+  email: string;
+  password: string;
+};
+
+export type UserSinginRes = {
+  token: string;
+};
