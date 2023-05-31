583f43278742022a3c651ecc130064b8f04ebf17
Yury <yuury.chang@gmail.com>
2023-05-31 18:33:43 +0800

    feat: Add example

--- src/app/demo/page.tsx
+++ src/app/demo/page.tsx
@@ -2,10 +2,14 @@
 
 /* eslint-disable no-console */
 import useDialogStore from '@/stores/dialogStore';
-import { Button } from '@chakra-ui/react';
+import useTicketPurchasingStore from '@/stores/ticketPurchasing';
+import { Button, Divider } from '@chakra-ui/react';
+import { useRouter } from 'next/navigation';
 
 const Demo = () => {
   const { openAlert, openConfirm } = useDialogStore();
+  const purchasingStore = useTicketPurchasingStore();
+  const router = useRouter();
 
   return (
     <div>
@@ -14,6 +18,16 @@ const Demo = () => {
       </Button>
       <Button type="button" onClick={() => openConfirm('confirm', () => console.log('hi confirm'))}>
         OpenConfirm
+      </Button>
+      <Divider my="24px" />
+      <Button
+        type="button"
+        onClick={() => {
+          purchasingStore.setEvent('activity', 'event');
+          router.push('/demo/process/1');
+        }}
+      >
+        start ticket purchasing process
       </Button>
     </div>
   );
