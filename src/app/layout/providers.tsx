781f81283b99a826d7a5af40ff7829f22a170f42
Yury <yuury.chang@gmail.com>
2023-06-01 16:39:46 +0800

    feat: Add SWR

--- src/app/layout/providers.tsx
+++ src/app/layout/providers.tsx
@@ -1,17 +1,26 @@
 'use client';
 
+import { SWRConfig } from 'swr';
+import type { SWRConfiguration } from 'swr';
 import { CacheProvider } from '@chakra-ui/next-js';
 import { ChakraProvider } from '@chakra-ui/react';
+import axiosClient from '@/api/axiosClient';
 import theme from './theme';
 
 type Props = {
   children: React.ReactNode;
 };
 
+const swrConfig: SWRConfiguration = {
+  fetcher: (resource, init) => axiosClient.get(resource, init).then((res) => res.data),
+};
+
 const Providers = ({ children }: Props) => {
   return (
     <CacheProvider>
-      <ChakraProvider theme={theme}>{children}</ChakraProvider>
+      <SWRConfig value={swrConfig}>
+        <ChakraProvider theme={theme}>{children}</ChakraProvider>
+      </SWRConfig>
     </CacheProvider>
   );
 };
