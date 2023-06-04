5a196283ff1e71ada78d428929fca4c3be377086
Yury <yuury.chang@gmail.com>
2023-06-04 12:35:52 +0800

    feat: Add Params arg to fetcher

--- src/api/swrFetcher.ts
+++ src/api/swrFetcher.ts
@@ -1,8 +1,26 @@
-import { SWRConfiguration } from 'swr';
+import { objectToUrlSearchParams } from '@/lib/tools';
 import axiosClient from './axiosClient';
 
-export const fetcher: SWRConfiguration['fetcher'] = (resource, init) =>
-  axiosClient.get(resource, init).then((res) => res.data?.data);
+type Args = string | [string, URLSearchParams | Record<string, any>];
 
-export const paginationFetcher: SWRConfiguration['fetcher'] = (resource, init) =>
-  axiosClient.get(resource, init).then((res) => res.data);
+const BaseFetcher = (args: Args) => {
+  let url = args;
+  let params: URLSearchParams | undefined;
+
+  if (Array.isArray(args)) {
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    const [_url, _params] = args;
+    url = _url;
+    if (_params instanceof URLSearchParams) {
+      params = _params;
+    } else {
+      params = objectToUrlSearchParams(_params);
+    }
+  }
+
+  return axiosClient.get(url as string, params && { params });
+};
+
+export const fetcher = (args: Args) => BaseFetcher(args).then((res) => res.data?.data);
+
+export const paginationFetcher = (args: Args) => BaseFetcher(args).then((res) => res.data);
