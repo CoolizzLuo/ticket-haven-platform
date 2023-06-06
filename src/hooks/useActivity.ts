c0b2e2805bc682eb18ba7425db1388f054b9ed0d
Yury <yuury.chang@gmail.com>
2023-06-06 14:54:34 +0800

    fix: Types

--- src/hooks/useActivity.ts
+++ src/hooks/useActivity.ts
@@ -11,7 +11,7 @@ export interface Event {
 export interface Activity {
   id: string;
   name: string;
-  converImageUrl: string;
+  coverImageUrl: string;
   startTime: string;
   endTime: string;
   location: string;
