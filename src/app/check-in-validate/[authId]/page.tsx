ed892b7a30b0ec7491096fff4c98231eb35abff5
Yury <yuury.chang@gmail.com>
2023-06-16 05:49:57 +0800

    feat: Add check-in validate page

---
+++ src/app/check-in-validate/[authId]/page.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import { useEventInfo } from '@/hooks/api/check-in/useEventInfo';
+import { Box, Skeleton } from '@/lib/chakra';
+import { useParams, useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+
+const CheckIn = () => {
+  const { authId } = useParams();
+  const router = useRouter();
+
+  const { event, isLoading } = useEventInfo(authId);
+
+  useEffect(() => {
+    if (!event && !isLoading) {
+      router.push('/');
+    }
+  }, [event, isLoading]);
+
+  console.log(event);
+  return (
+    <Skeleton minH="300px" isLoaded={!isLoading}>
+      <Box h="200px" bgImage={event?.coverImgUrl} bgSize="cover" bgPosition="center" />
+    </Skeleton>
+  );
+};
+
+export default CheckIn;
--- src/lib/chakra.ts
+++ src/lib/chakra.ts
@@ -1,3 +1,5 @@
 'use client';
 
 export * from '@chakra-ui/react';
+export { CacheProvider, Link, Image } from '@chakra-ui/next-js';
+export type { CacheProviderProps, LinkProps, ImageProps } from '@chakra-ui/next-js';
