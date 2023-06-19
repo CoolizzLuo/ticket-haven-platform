852b4464f87a4a3f0b7a816ea76216faa2cec696
PageShih <a0981863141@gmail.com>
2023-06-20 02:14:07 +0800

    fix: Reorder function & add breadcrumb

--- src/app/(main)/purchasing-process/(reservation)/layout.tsx
+++ src/app/(main)/purchasing-process/(reservation)/layout.tsx
@@ -3,18 +3,41 @@
 import useTicketPurchasingStore from '@/stores/ticketPurchasing';
 import { useRouter } from 'next/navigation';
 import { ReactNode, useEffect } from 'react';
+import useActivity from '@/hooks/api/useActivity';
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink } from '@chakra-ui/react';
 
 const Layout = ({ children }: { children: ReactNode }) => {
   const activityId = useTicketPurchasingStore.use.activityId();
+  const setActivity = useTicketPurchasingStore.use.setActivity();
   const router = useRouter();
+  const { activity } = useActivity(activityId);
 
   useEffect(() => {
     if (!activityId) {
       router.push('/');
+    } else if (activity) {
+      setActivity(activity);
     }
-  }, []);
+  }, [activityId, activity]);
 
-  return children;
+  return (
+    <>
+      <Breadcrumb style={{ padding: '30px 20px' }}>
+        <BreadcrumbItem>
+          <BreadcrumbLink href="/">首頁 </BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbItem>
+          <BreadcrumbLink href={`/activities/${activityId}`}>{activity?.name}</BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbItem>
+          <BreadcrumbLink href="#" fontWeight="700">
+            購票流程
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+      </Breadcrumb>
+      {children}
+    </>
+  );
 };
 
 export default Layout;
--- src/app/(main)/purchasing-process/(reservation)/select-area/page.tsx
+++ src/app/(main)/purchasing-process/(reservation)/select-area/page.tsx
@@ -4,7 +4,6 @@ import { useState } from 'react';
 import StepPage from '@/components/activity/step';
 import useTicketPurchasingStore from '@/stores/ticketPurchasing';
 import SeatSelector from '@/components/activity/step/SeatsSelector';
-import useActivity from '@/hooks/api/useActivity';
 import useSeatAreas from '@/hooks/useSeatAreas';
 import { Area } from '@/types/activityTypes';
 import axiosClient from '@/api/axiosClient';
@@ -13,7 +12,7 @@ const SelectArea = () => {
   const activityId = useTicketPurchasingStore.use.activityId();
   const eventId = useTicketPurchasingStore.use.eventId();
   const setEvent = useTicketPurchasingStore.use.setEvent();
-  const { activity } = useActivity(activityId);
+  const activity = useTicketPurchasingStore.use.activity();
   const { seatAreas, mutate } = useSeatAreas(eventId);
   const [seats, setSeats] = useState<Area[]>([]);
 
